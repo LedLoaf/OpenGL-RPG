06d51476a2a3e2ce1c7163cf30a07c3b98fb2561
LedLoaf <LedLoaf@comcast.net>
2022-03-14 19:34:38 -0700

    Batch Renderer start

---
+++ Engine/src/Material.cpp
@@ -0,0 +1,17 @@
+﻿#include "Material.h"
+
+GLuint Material::m_idGen = 0u;
+
+void Material::compile()
+{
+	// Set up the tex unit to the correct value
+	shader.use();
+	shader.setInt("image", m_texIndex);
+}
+
+void Material::bind()
+{
+	// Activate defined tex unit and bind appropriate texture onto it
+	glActiveTexture(GL_TEXTURE0 + m_texIndex);
+	texture.bind();
+}
---
+++ Engine/src/Material.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Shader.h"
+#include "Texture.h"
+
+// Abstract holding class for texture shader combinations used for rendering
+class Material
+{
+public:
+	Material(Texture& texture, Shader& shader, const GLint texUnit)
+		: texture{texture},
+		  shader{shader},
+		  id{m_idGen++},
+		  m_texIndex{texUnit} { }
+
+	~Material() = default;
+
+	// Takes and uses shader and sets up texture unit
+	void compile();
+
+	void bind();
+
+public:
+	Texture& texture;
+	Shader&  shader;
+	GLuint   id;
+private:
+	static GLuint m_idGen;
+	GLint         m_texIndex;
+};
